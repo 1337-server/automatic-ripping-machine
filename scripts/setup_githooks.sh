61f4f5d383d8b129f4a88f55a3362d4b34849133
wolfy <wolfy@shitwolfymakes.com>
2022-03-31 22:31:15 -0500

    set config for local repo

--- scripts/setup_githooks.sh
+++ scripts/setup_githooks.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# set the location of the git hooks for this repo only
+git config --local core.hooksPath ".git/hooks"
+
+# symlink all the provided links to make them usable
 ln -sf .githooks/* .git/hooks/
