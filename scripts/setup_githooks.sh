627a4a1789529e27de7257c0c8309aeca62e1d6d
wolfy <wolfy@shitwolfymakes.com>
2022-04-11 19:38:35 -0500

    removed githooks

--- scripts/setup_githooks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# set the location of the git hooks for this repo only
-git config --local core.hooksPath ".git/hooks"
-
-# symlink all the provided links to make them usable
-ln -sf .githooks/* .git/hooks/
